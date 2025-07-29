44b0a340c82f3c423b03c0420a17865226d8dcb9
MaLevin <marinal1805@gmail.com>
2025-07-29 19:19:12 +0300

    css Selectors

--- src/test/java/experiments/CssSelectors.java
+++ src/test/java/experiments/CssSelectors.java
@@ -1,6 +1,7 @@
 package experiments;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,6 +25,44 @@ public class CssSelectors {
         System.out.println("Attribute element -->" + imgTools.getAttribute("src"));
 
         pause(4000);
+
+       WebElement divElements = driver.findElement(By.cssSelector("div[class='card-up']"));
+       // WebElement divElements = driver.findElement(By.cssSelector(".card-up"));
+        divElements.click();
+
+       // WebElement btnRadioButton =driver.findElement(By.cssSelector("#item-2"));
+        WebElement btnRadioButton =driver.findElement(By.id("item-2"));
+        btnRadioButton.click();
+
+        //WebElement radioButtonYes = driver.findElement(By.id("yesRadio"));
+        //WebElement radioButtonYes = driver.findElement(By.cssSelector("label[for='yesRadio']"));
+        WebElement radioButtonYes1 = driver.findElement(By.cssSelector("*[for='yesRadio']"));
+        WebElement radioButtonYes = driver.findElement(By.xpath("//label[@for='yesRadio']"));
+        radioButtonYes.click();
+        pause(4000);
+
+       driver.navigate().back();
+       driver.navigate().back();
+
+       hideBanner();
+       hideBanner();
+
+       WebElement divBookStore = driver.findElement
+               (By.cssSelector("div[class='category-cards'] div[class='card mt-4 top-card']:last-child"));
+       divBookStore.click();
+        pause(4000);
+
+        driver.navigate().back();
+
+        WebElement divWidgets = driver.findElement
+                (By.cssSelector("div[class='category-cards'] div[class='card mt-4 top-card']:nth-child(4) div[class='card-body'] h5"));
+        System.out.println(divWidgets.getText());
+
+        driver.navigate().back();
+        pause(4000);
+
+
+
         driver.quit();
         //driver.close();
     }
@@ -35,4 +74,15 @@ public class CssSelectors {
             throw new RuntimeException(e);
         }
     }
+
+    public void hideBanner(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('#fixedban').style.display='none'");
+    }
+
+    public void hideFooter(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('footer').style.display='none'");
+    }
+
 }
